c8468ce58279d24da9232893ccae0257d67f4a81
Li Wang <wangfan8@gmail.com>
2017-10-24 11:23:17 -0400

    add data processing for test data too

--- 01_feature_engineering.R
+++ 01_feature_engineering.R
@@ -2,4 +2,10 @@ library(tidyverse)
 
 train_data <- read_rds("~/Dropbox/kkbox/data/merged_train.rds")
 
+set.seed(100)
+
+
+
+
+
 
--- init_exploration.R
+++ init_exploration.R
@@ -8,6 +8,7 @@ sample_submission <- read_csv("sample_submission.csv")
 song_extra_info   <- read_csv("song_extra_info.csv")
 songs <- read_csv("songs.csv")
 train <- read_csv("train.csv")
+test  <- read_csv("test.csv") 
 
 dim(train)
 length(unique(train$msno))
@@ -32,7 +33,27 @@ train_data <- train_data %>%
                      = ymd(as.character(registration_init_time))) %>%
               mutate(expiration_date
                      = ymd(as.character(expiration_date)))
-              
+
+# use same linking/cleaning methods on test data
+test_data <- songs %>% 
+  left_join(song_extra_info, by = "song_id") %>%
+  right_join(test, by = "song_id") %>%
+  left_join(members, by = "msno")
+
+# clean up data with wrong data type and obvious outliers
+test_data <- test_data %>%
+  mutate(city = as.character(city)
+         , language = as.character(language)
+         , registered_via = as.character(registered_via)) %>%
+  mutate(bd = ifelse(bd<=0 | bd > 100, NA, bd)) %>%
+  mutate(song_length = ifelse(song_length/6000 > 1440
+                              , 1440
+                              , song_length/6000)) %>%
+  mutate(registration_init_time 
+         = ymd(as.character(registration_init_time))) %>%
+  mutate(expiration_date
+         = ymd(as.character(expiration_date)))
+
 # initial exploration on one customer
 
 c1 <- train_data %>% 
@@ -48,7 +69,8 @@ unique_and_total_songs <- train_data %>%
                                     , repeat_songs = sum(target)) %>%
                           mutate(repeat_ratio      = repeat_songs / total_songs)
 
-saveRDS(train_data, "~/Dropbox/kkbox/data/merged_train.rds")                         
+saveRDS(train_data, "~/Dropbox/kkbox/data/merged_train.rds") 
+saveRDS(test_data, "~/Dropbox/kkbox/data/merged_test.rds")
 
              
 
